e9b104226885da3516de3b364eb97f8251f436fc
Razia Pervez <160026294+RaziaPervez@users.noreply.github.com>
2024-03-09 10:31:29 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+//project=21
+var itCourse = {
+    courseName: "typescript and javascript",
+    location: "Goveror House Sindh",
+    onsiteStudent: 50000,
+};
+console.log(itCourse);
---
+++ main.ts
@@ -0,0 +1,16 @@
+//project=21
+
+//They think of something you could store in a TypeScript Object. 
+//Write a program that creates Objects containing these items.
+
+interface itCourse{
+    courseName:string;
+    location:string;
+    onsiteStudend:number;
+}
+let itCourse={
+    courseName:"typescript and javascript",
+    location:"Goveror House Sindh",
+    onsiteStudent:50000,
+};
+console.log(itCourse)
